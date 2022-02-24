08ac0479a1bf74f89f568bcc7e8fcbccb796868c
Roy Christian Pery <thercpery@gmail.com>
2022-02-24 18:58:45 +0800

    change response

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => res.send("28-3"))
+app.get("/", (req, res) => res.send("Hello world"));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
