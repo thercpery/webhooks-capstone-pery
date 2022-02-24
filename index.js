08aea177e7df172f6c27844892b637f49c64b91f
Roy Christian Pery <thercpery@gmail.com>
2022-02-24 18:57:12 +0800

    added route

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const PORT = process.port.ENV || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/")
+app.get("/", (req, res) => res.send("28-3"))
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
