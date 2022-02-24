b6b26c746dc2250e2923488235fd44847b47b2a5
Roy Christian Pery <thercpery@gmail.com>
2022-02-24 21:00:33 +0800

    removed req

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => res.send("Hello world"));
 
 app.post("/", (req, res) => {
-    res.status(200).send(req);
+    res.send(req);
 });
 
 app.get("/result", (req, res) => {
